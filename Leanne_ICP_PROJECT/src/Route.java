952a3db9968c5cfeea754223cc20cd5b50416522
Leanne Annor-Adjaye <60921784+MaameMozuma@users.noreply.github.com>
2022-10-01 13:27:45 +0000

    Update Route.java

--- Leanne_ICP_PROJECT/src/Route.java
+++ Leanne_ICP_PROJECT/src/Route.java
@@ -16,7 +16,26 @@ public class Route {
     private String Stops;
     private String Equipment;
 
-    public Route(String airline_Code, String airline_ID, String source_Airport_Code, String source_Airport_ID, String destination_Airport_Code, String destination_Airport_ID, String codeshare, String stops, String equipment) {
+    /**
+     * @param airline_Code
+     * @param airline_ID
+     * @param source_Airport_Code
+     * @param source_Airport_ID
+     * @param destination_Airport_Code
+     * @param destination_Airport_ID
+     * @param codeshare
+     * @param stops
+     * @param equipment
+     */
+    public Route(String airline_Code,
+                 String airline_ID,
+                 String source_Airport_Code,
+                 String source_Airport_ID,
+                 String destination_Airport_Code,
+                 String destination_Airport_ID,
+                 String codeshare,
+                 String stops,
+                 String equipment) {
         this.Airline_Code = airline_Code;
         this.Airline_ID = airline_ID;
         this.Source_Airport_Code = source_Airport_Code;
@@ -28,38 +47,24 @@ public class Route {
         this.Equipment = equipment;
     }
 
-    /**
-     * The toString() method returns a string representation of the object
-     *
-     * @return The toString method is being returned.
-    /**
-     * This function returns the Airline Code of the flight
-     *
-     * @return The Airline_Code
-     */
-
 
     /**
      * The toString() method returns a string representation of the object
-     *
      * @return The toString method is being returned.
      */
     public String toString() {
-        return "Route{Airline_Code='" + this.Airline_Code + "', Airline_ID='" + this.Airline_ID + "', Source_Airport_Code='" + this.Source_Airport_Code + "', Source_Airport_ID='" + this.Source_Airport_ID + "', Destination_Airport_Code='" + this.Destination_Airport_Code + "', Destination_Airport_ID='" + this.Destination_Airport_ID + "', Codeshare='" + this.Codeshare + "', Stops='" + this.Stops + "', Equipment='" + this.Equipment + "'}";
+        return "Route{Airline_Code='" +
+                this.Airline_Code + "', Airline_ID='" +
+                this.Airline_ID + "', Source_Airport_Code='" +
+                this.Source_Airport_Code + "', Source_Airport_ID='" +
+                this.Source_Airport_ID + "', Destination_Airport_Code='" +
+                this.Destination_Airport_Code + "', Destination_Airport_ID='" +
+                this.Destination_Airport_ID + "', Codeshare='" + this.Codeshare +
+                "', Stops='" + this.Stops + "', Equipment='" + this.Equipment + "'}";
     }
 
     /**
      * This function returns the Airline Code of the flight
-     *
-     * @return The Airline_Code
-     */
-    /**
-     */
-
-
-    /**
-     * This function returns the Airline Code of the flight
-     *
      * @return The Airline_Code
      */
     public String getAirline_Code() {
@@ -68,21 +73,23 @@ public class Route {
 
     /**
      * This function returns the source airport code of the flight
-     *
      * @return The source airport code.
      */
     public String getSource_Airport_Code() {
         return this.Source_Airport_Code;
     }
 
-    // Returning the destination airport code of the flight.
+
+    /**
+     * This function returns the Destination_Airport_Code of the Flight object
+     * @return The Destination_Airport_Code
+     */
     public String getDestination_Airport_Code() {
         return this.Destination_Airport_Code;
     }
 
     /**
      * This function returns the stops of the flight
-     *
      * @return The stops
      */
     public String getStops() {
@@ -90,9 +97,9 @@ public class Route {
     }
 
     /**
-     * If the object is not null and the object is of the same class as the current object, then check if the values of the
+     * If the object is not null and the object is of the same class as the current
+     * object, then check if the values of the
      * fields are equal
-     *
      * @param o The object to be compared for equality with this route.
      * @return The hashcode of the object.
      */
@@ -101,7 +108,15 @@ public class Route {
             return true;
         } else if (o != null && this.getClass() == o.getClass()) {
             Route route = (Route)o;
-            return Objects.equals(this.Airline_Code, route.Airline_Code) && Objects.equals(this.Airline_ID, route.Airline_ID) && Objects.equals(this.Source_Airport_Code, route.Source_Airport_Code) && Objects.equals(this.Source_Airport_ID, route.Source_Airport_ID) && Objects.equals(this.Destination_Airport_Code, route.Destination_Airport_Code) && Objects.equals(this.Destination_Airport_ID, route.Destination_Airport_ID) && Objects.equals(this.Codeshare, route.Codeshare) && Objects.equals(this.Stops, route.Stops) && Objects.equals(this.Equipment, route.Equipment);
+            return Objects.equals(this.Airline_Code, route.Airline_Code) &&
+                    Objects.equals(this.Airline_ID, route.Airline_ID) &&
+                    Objects.equals(this.Source_Airport_Code, route.Source_Airport_Code)
+                    && Objects.equals(this.Source_Airport_ID, route.Source_Airport_ID) &&
+                    Objects.equals(this.Destination_Airport_Code, route.Destination_Airport_Code)
+                    && Objects.equals(this.Destination_Airport_ID, route.Destination_Airport_ID)
+                    && Objects.equals(this.Codeshare, route.Codeshare)
+                    && Objects.equals(this.Stops, route.Stops)
+                    && Objects.equals(this.Equipment, route.Equipment);
         } else {
             return false;
         }
@@ -109,10 +124,13 @@ public class Route {
 
     /**
      * The hashCode() function is used to return the hash code value for this object
-     *
      * @return The hashcode of the object.
      */
     public int hashCode() {
-        return Objects.hash(new Object[]{this.Airline_Code, this.Airline_ID, this.Source_Airport_Code, this.Source_Airport_ID, this.Destination_Airport_Code, this.Destination_Airport_ID, this.Codeshare, this.Stops, this.Equipment});
+        return Objects.hash(this.Airline_Code,
+                this.Airline_ID,
+                this.Source_Airport_Code, this.Source_Airport_ID,
+                this.Destination_Airport_Code, this.Destination_Airport_ID,
+                this.Codeshare, this.Stops, this.Equipment);
     }
 }
