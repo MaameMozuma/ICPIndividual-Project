ffbff4d1fffc60f314d7cad995e24cc697274f18
Leanne Annor-Adjaye <60921784+MaameMozuma@users.noreply.github.com>
2022-10-01 13:20:37 +0000

    Update Airport.java

--- Leanne_ICP_PROJECT/src/Airport.java
+++ Leanne_ICP_PROJECT/src/Airport.java
@@ -19,7 +19,36 @@ public class Airport {
     private String Type;
     private String Source;
 
-    public Airport(int airport_ID, String name, String city, String country, String IATA_Code, String ICAO_Code, float latitude, float longitude, int altitude, float timezone, String DST, String tz_database_time_zone, String type, String source) {
+    /**
+     * @param airport_ID
+     * @param name
+     * @param city
+     * @param country
+     * @param IATA_Code
+     * @param ICAO_Code
+     * @param latitude
+     * @param longitude
+     * @param altitude
+     * @param timezone
+     * @param DST
+     * @param tz_database_time_zone
+     * @param type
+     * @param source
+     * */
+    public Airport(int airport_ID,
+                   String name,
+                   String city,
+                   String country,
+                   String IATA_Code,
+                   String ICAO_Code,
+                   float latitude,
+                   float longitude,
+                   int altitude,
+                   float timezone,
+                   String DST,
+                   String tz_database_time_zone,
+                   String type,
+                   String source) {
         this.Airport_ID = airport_ID;
         this.Name = name;
         this.City = city;
@@ -36,13 +65,6 @@ public class Airport {
         this.Source = source;
     }
 
-    public String getName() {
-        return this.Name;
-    }
-
-    public int getAirport_ID() {
-        return this.Airport_ID;
-    }
 
     public String getCity() {
         return this.City;
@@ -56,29 +78,26 @@ public class Airport {
         return this.IATA_Code;
     }
 
-    public String getICAO_Code() {
-        return this.ICAO_Code;
-    }
-
-    public float getLatitude() {
-        return this.Latitude;
-    }
-
-    public float getLongitude() {
-        return this.Longitude;
-    }
-
     /**
      * The toString() method returns a string representation of the object
      *
      * @return The toString method is being returned.
      */
     public String toString() {
-        return "Airport{Airport_ID=" + this.Airport_ID + ", Name='" + this.Name + "', City='" + this.City + "', Country='" + this.Country + "', IATA_Code='" + this.IATA_Code + "', ICAO_Code='" + this.ICAO_Code + "', Latitude=" + this.Latitude + ", Longitude=" + this.Longitude + ", Altitude=" + this.Altitude + ", Timezone=" + this.Timezone + ", DST='" + this.DST + "', Tz_database_time_zone='" + this.Tz_database_time_zone + "', Type='" + this.Type + "', Source='" + this.Source + "'}";
+        return "Airport{Airport_ID=" +
+                this.Airport_ID +
+                ", Name='" + this.Name + "', City='" + this.City + "', Country='" +
+                this.Country +
+                "', IATA_Code='" + this.IATA_Code + "', ICAO_Code='" + this.ICAO_Code +
+                "', Latitude=" + this.Latitude + ", Longitude=" + this.Longitude +
+                ", Altitude=" + this.Altitude + ", Timezone=" + this.Timezone +
+                ", DST='" + this.DST + "', Tz_database_time_zone='" + this.Tz_database_time_zone +
+                "', Type='" + this.Type + "', Source='" + this.Source + "'}";
     }
 
     /**
-     * If the object is the same object, return true. If the object is not an Airport object, return false. If the object
+     * If the object is the same object, return true.
+     * If the object is not an Airport object, return false. If the object
      * is an Airport object, compare the Airport_ID's
      *
      * @param obj The object to compare this Airport against.
